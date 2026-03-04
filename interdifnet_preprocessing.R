e4be97190d6e940ffd0b8a5a5be9ae92e1f64da0
Yale Quan <yalequan@gmail.com>
2026-03-03 17:04:37 -0800

    Update interdifnet_preprocessing.R

--- interdifnet_preprocessing.R
+++ interdifnet_preprocessing.R
@@ -5,7 +5,7 @@
 # Supports ONLY 3-group and 10-group configurations.
 # =============================================================================
 
-preprocess_interdifnet <- function(
+interdifnet_preprocessing <- function(
     item_responses,
     group_assignments,
     num_groups = NULL,
